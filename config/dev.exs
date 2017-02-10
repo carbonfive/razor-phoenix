4448a4c2cbbbd2f4a615a83d96a968e376cff774
Craig Lyons <craiglyons.dev@gmail.com>
2017-02-10 11:49:43 -0500

    Add slim support, convert eex templates, add flash demo

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :app_prototype, AppPrototype.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|slim)$}
     ]
   ]
 
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule AppPrototype.PageController do
   use AppPrototype.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "This is a demo info flash, hard-coded in PageController.")
+    |> put_flash(:error, "This is a demo error flash, hard-coded in PageController.")
+    |> render "index.html"
   end
 end
