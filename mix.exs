a4b94cc83845d0a4289f35284d6a56719e3451d0
Craig Lyons <craiglyons.dev@gmail.com>
2017-07-12 10:12:39 -0400

    Upgrade elixir to 1.5.1, upgrade to CircleCI 2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AppPrototype.Mixfile do
   def project do
     [app: :app_prototype,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.5.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
--- script/ci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export MIX_ENV="test"
-export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
-
-brunch build
-mix test
