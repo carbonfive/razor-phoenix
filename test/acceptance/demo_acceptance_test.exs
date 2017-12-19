28512d34eea88e8f0a71d51c5510253bf2972d81
Andrew Hao <andrew@carbonfive.com>
2017-12-19 10:42:10 -0800

    Update Wallaby test to use 0.19 syntax

--- test/acceptance/demo_acceptance_test.exs
+++ test/acceptance/demo_acceptance_test.exs
@@ -2,11 +2,9 @@ defmodule AppPrototype.UserListTest do
   use AppPrototype.AcceptanceCase, async: true
 
   test "javascript is working", %{session: session} do
-    js_demo_text =
-      session
-      |> visit("/")
-      |> find("#js-demo")
-      |> text
-    assert String.contains?(js_demo_text, "Brunch with custom js is working")
+    session
+    |> visit("/")
+    |> find(Query.css("#js-demo"))
+    |> assert_text("Brunch with custom js is working")
   end
 end
