da3e69c9ccf912fb35fb41dc9588b326e5015951
cavewoman <anna.chan365@gmail.com>
2018-01-05 17:05:56 -0500

    ISSUE-28 [Anna] Adds secret key base to dev and prod envs endpoints

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,7 @@ use Mix.Config
 config :app_prototype, AppPrototypeWeb.Endpoint,
   load_from_system_env: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
